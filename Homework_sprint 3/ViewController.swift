892dd7ce91a71e118dc95438b62d433d83efe049
Shishmakova Daria <dary_shishka@mail.ru>
2022-10-05 12:43:09 +0300

    Initial Commit

--- Homework_sprint 3/ViewController.swift
+++ Homework_sprint 3/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var counter: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    private var count = 0
+    
+    @IBAction func buttonDidPush(_ sender: Any) {
+            count += 1
+            counter.text = "Значение счётчика:\n\(count)"
+    }
 }
 
